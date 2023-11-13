ae53691168cfb508b164c77cc477b3260d360ee1
Sean Wong <seanwong24@live.com>
2023-11-13 10:33:09 +0000

    minimal implmentation of general info page

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 export let API_ENDPOINT: string | null = null;
 
 if (!API_ENDPOINT) {
-  API_ENDPOINT = prompt("Enter the API endpoint.", "http://localhost:8000/api") ?? "";
+  API_ENDPOINT =
+    prompt("Enter the API endpoint.", "http://localhost:8000/api") ?? "";
 }
 
 export async function checkAuthentication() {
@@ -26,5 +27,49 @@ export async function signIn(passcode: string) {
   await fetch(`${API_ENDPOINT}/auth/sign-in?passcode=${passcode}`, {
     method: "POST",
     credentials: "include",
+  });
+}
+
+export async function obtainGeneralInfoList() {
+  return await fetch(`${API_ENDPOINT}/general`).then((response) =>
+    response.json()
+  );
+}
+
+export async function obtainGeneralInfo(key: string) {
+  return await fetch(`${API_ENDPOINT}/general/${encodeURIComponent(key)}`).then(
+    async (response) => {
+      const text = await response.text();
+      try {
+        return JSON.parse(text);
+      } finally {
+        return text;
+      }
+    }
+  );
+}
+
+export async function deleteGeneralInfo(key: string) {
+  return await fetch(`${API_ENDPOINT}/general/${encodeURIComponent(key)}`, {
+    method: "DELETE",
+    credentials: "include",
+  });
+}
+
+export async function addGeneralInfo(key: string, value: unknown) {
+  return await fetch(`${API_ENDPOINT}/general/${encodeURIComponent(key)}`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(value),
+    headers: new Headers({ "content-type": "application/json" }),
+  });
+}
+
+export async function updateGeneralInfo(key: string, value: unknown) {
+  return await fetch(`${API_ENDPOINT}/general/${encodeURIComponent(key)}`, {
+    method: "PUT",
+    credentials: "include",
+    body: JSON.stringify(value),
+    headers: new Headers({ "content-type": "application/json" }),
   });
 }
--- src/views/GeneralInfoPage.vue
+++ src/views/GeneralInfoPage.vue
@@ -2,32 +2,140 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>General Info</ion-title>
+        <ion-title size="large">General Info</ion-title>
+        <ion-buttons slot="end" :collapse="true">
+          <ion-button @click="addItemHandler">
+            <ion-icon slot="icon-only" :icon="add"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+      <ion-header data-note="This is only for iOS." collapse="condense">
         <ion-toolbar>
           <ion-title size="large">General Info</ion-title>
+          <ion-buttons slot="end" :collapse="true">
+            <ion-button @click="addItemHandler">
+              <ion-icon slot="icon-only" :icon="add"></ion-icon>
+            </ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="General Info page" />
+      <ion-list>
+        <ion-item
+          v-for="itemKey in itemKeys"
+          button
+          @click="showItemDetailHandler(itemKey)"
+        >
+          {{ itemKey }}
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import {
+  IonButton,
+  IonButtons,
   IonPage,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonList,
   IonToolbar,
   IonTitle,
   IonContent,
+  alertController,
 } from "@ionic/vue";
 import { useRouter } from "vue-router";
-import ExploreContainer from "@/components/ExploreContainer.vue";
-import { redirectToMePageIfNotAuthenticated } from "@/utils/api";
+import {
+  addGeneralInfo,
+  deleteGeneralInfo,
+  obtainGeneralInfo,
+  obtainGeneralInfoList,
+  redirectToMePageIfNotAuthenticated,
+  updateGeneralInfo,
+} from "@/utils/api";
+import { onBeforeMount, ref } from "vue";
+import { add } from "ionicons/icons";
 
 redirectToMePageIfNotAuthenticated(useRouter().replace);
+
+const itemKeys = ref<string[]>();
+
+const refreshList = async () => {
+  itemKeys.value = await obtainGeneralInfoList();
+};
+
+onBeforeMount(async () => {
+  await refreshList();
+});
+
+const addItemHandler = async () => {
+  const alert = await alertController.create({
+    header: "Adding an entry",
+    inputs: [
+      {
+        name: "key",
+        placeholder: "Key",
+      },
+      {
+        name: "value",
+        placeholder: "Value",
+      },
+    ],
+    buttons: [
+      {
+        text: "Cancel",
+        role: "cancel",
+      },
+      {
+        text: "OK",
+        role: "confirm",
+        handler: async ({ key, value }) => {
+          await addGeneralInfo(key, value);
+          await refreshList();
+        },
+      },
+    ],
+  });
+  await alert.present();
+  await refreshList();
+};
+
+const showItemDetailHandler = async (key: string) => {
+  const alert = await alertController.create({
+    header: "Adding an entry",
+    inputs: [
+      {
+        name: "value",
+        value: JSON.stringify(await obtainGeneralInfo(key)),
+      },
+    ],
+    buttons: [
+      {
+        text: "Delete",
+        handler: async () => {
+          await deleteGeneralInfo(key);
+          await refreshList();
+        },
+      },
+      {
+        text: "Update",
+        handler: async ({ value }) => {
+          await updateGeneralInfo(key, value);
+          await refreshList();
+        },
+      },
+      {
+        text: "OK",
+        role: "cancel",
+      },
+    ],
+  });
+  await alert.present();
+  await refreshList();
+};
 </script>

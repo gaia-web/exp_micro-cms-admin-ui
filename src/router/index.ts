eef3e815f9a89be80b7cfef95d89f5f54915cb70
Sean Wong <seanwong24@live.com>
2023-11-13 06:16:39 +0000

    update tab list

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/me'
   },
   {
     path: '/tabs/',
@@ -13,19 +13,23 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/me'
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: 'general',
+        component: () => import('@/views/GeneralInfoPage.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: 'documents',
+        component: () => import('@/views/DocumentsPage.vue')
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'assets',
+        component: () => import('@/views/AssetsPage.vue')
+      },
+      {
+        path: 'me',
+        component: () => import('@/views/MePage.vue')
       }
     ]
   }
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,24 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="general" href="/tabs/general">
+          <ion-icon aria-hidden="true" :icon="informationCircle" />
+          <ion-label>General Info</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="documents" href="/tabs/documents">
+          <ion-icon aria-hidden="true" :icon="documentText" />
+          <ion-label>Documents</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="assets" href="/tabs/assets">
+          <ion-icon aria-hidden="true" :icon="attach" />
+          <ion-label>Assets</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="me" href="/tabs/me">
+          <ion-icon aria-hidden="true" :icon="person" />
+          <ion-label>Me</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +29,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { attach, documentText, informationCircle, person } from 'ionicons/icons';
 </script>

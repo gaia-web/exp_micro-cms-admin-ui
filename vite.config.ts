2c351cf79e6061faa9885950d3ed029ccb17d0e5
Sean Wong <seanwong24@live.com>
2024-02-17 07:12:52 +0000

    add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,39 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Deno MicroCMS Admin",
+        short_name: "DMC Admin",
+        start_url: "./",
+        icons: [
+          {
+            src: "./icon.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+    legacy(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+  base: "./",
+});
